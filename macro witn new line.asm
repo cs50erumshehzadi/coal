b8df18336460a581ce08c3e2a56d18a13f6f4831
cs50erumshehzadi <75004514+cs50erumshehzadi@users.noreply.github.com>
2022-01-01 10:24:12 +0500

    1/1/22

---
+++ macro witn new line.asm
@@ -0,0 +1,52 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+    ; stack  topic
+    ;stack in proc
+    ; with use
+
+; add your code here 
+; macro
+printfunc macro p1
+    mov dx,offset p1
+    mov ah ,9
+    int 21h 
+    endm
+newline macro p1
+    mov dx,10
+    mov ah ,2
+    int 21h
+    mov dx,13
+    mov ah ,2
+    int 21h 
+    endm
+    
+    
+    
+    
+    
+
+.model small
+.stack 100h
+.data 
+msg1 db  " erum $"
+msg2 db " ali $" 
+
+.code 
+main proc
+    mov ax,@data
+    mov ds,ax
+    printfunc msg1
+    newline 
+    
+    printfunc msg2
+    main endp
+end
+
+
+
+
+
+   
+
+
